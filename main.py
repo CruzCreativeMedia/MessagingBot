990611e2e0d20b3b05b3fda1223f590e18220dc0
Cruz Creative Media <79482911+CruzCreativeMedia@users.noreply.github.com>
2021-08-22 19:53:32 +0000

    Time and Hello Message

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ client=slack.WebClient(token=os.environ['slack_token'])
 
 def messageMe():
   print ("done")
-  client.chat_postMessage(channel="dev-slack-apps",text="Hey! Agata I like you")
+  client.chat_postMessage(channel="dev-slack-apps",text="Hello World!")
 
-schedule.every().sunday.at("19:48").do(messageMe)
+schedule.every().sunday.at("20:00").do(messageMe)
 print('Script is Run')
 
 while True:
